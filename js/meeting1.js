d47427fa6fd1991505572ef4f1270e5ce92c9463
Anton K <akarpenko83@gmail.com>
2023-01-28 12:54:36 +0200

    task #3

--- js/meeting1.js
+++ js/meeting1.js
@@ -26,26 +26,59 @@
 // Напишите цикл, который предлагает ввести
 // число больше 100 через
 
-let userInput = Number(prompt("введите число больше 100"));
-let check;
-//   alert("Please enter number");
-do {
-  //   if (isNaN(userInput) || userInput < 100) {
-  //     check = true;
-  //     userInput = Number(prompt("Your data is invalid"));
-  //   } else {
-  //     check = false;
-  //     alert("You enter 100");
-  //   }
+// let userInput = Number(prompt("введите число больше 100"));
+// let check;
+// //   alert("Please enter number");
+// do {
+//   //   if (isNaN(userInput) || userInput < 100) {
+//   //     check = true;
+//   //     userInput = Number(prompt("Your data is invalid"));
+//   //   } else {
+//   //     check = false;
+//   //     alert("You enter 100");
+//   //   }
 
-  if (userInput > 100) {
-    check = false;
-    alert("You enter 100 or more");
-  } else if (isNaN(userInput)) {
-    check = true;
-    userInput = Number(prompt("It's not a number"));
-  } else {
-    check = true;
-    userInput = Number(prompt("Your data is invalid"));
-  }
-} while (check);
+//   if (userInput > 100) {
+//     check = false;
+//     alert("You enter 100 or more");
+//   } else if (isNaN(userInput)) {
+//     check = true;
+//     userInput = Number(prompt("It's not a number"));
+//   } else {
+//     check = true;
+//     userInput = Number(prompt("Your data is invalid"));
+//   }
+// } while (check);
+
+
+// ------------------------------------------- task 3 --------------------------------
+// Запрашиваем у пользователя количество итераций и числа,
+//    если четные добавляем в Тотал, нечетные пропускаем,
+//    после последней итерации выводим тотал
+
+
+  
+// let userIterr = Number(prompt("Введіть кількість ітерацій"));
+// let total = 0;
+
+// if (!isNaN(userIterr)) {
+//    for (let i = 0; i < userIterr; i++) {
+//       let userInput = Number(prompt("Введіть число"));
+//       if ((isNaN(userInput))) {
+//          alert("Ви ввели невалідне число. Не зараховано")
+//          continue;
+//       } else if ((userInput % 2 !== 0)) {
+//          alert("Ваше число непарне. Не зараховано");
+//          continue;
+//       } else {
+//          alert(`Парне! Зараховано! Вітаю`);
+//          total += userInput;
+//          console.log(total);
+//       }
+//    }
+// } else {
+//    alert("Ви ввели не валідні дані. Нажміть F5");
+//    // userInput = Number(prompt("Введіть число")); 
+//  }
+// console.log(`Фінальний результат ${total}, кількість ітерацій ${userIterr}`);
+
