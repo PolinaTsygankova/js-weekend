c2f2491cc831a77ffb31ff24993c7a14b78557c9
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-02-25 13:46:18 +0200

    add last task 25.02

--- js/meeting03.js
+++ js/meeting03.js
@@ -33,12 +33,12 @@
 
 //? Напишіть функцію для нормалізації дати
 
-const inputData = [
-  { id: 1, name: 'Item 1' },
-  { id: 2, name: 'Item 2' },
-  { id: 4, name: 'Item 4' },
-  { id: 7, name: 'Item 7' },
-];
+// const inputData = [
+//   { id: 1, name: 'Item 1' },
+//   { id: 2, name: 'Item 2' },
+//   { id: 4, name: 'Item 4' },
+//   { id: 7, name: 'Item 7' },
+// ];
 
 // ('function should return normalized data', () => {
 //     expect(normalize(inputData)).toEqual({
@@ -57,24 +57,56 @@ const inputData = [
 //4 створюемо поле для обьекту дати по ключу айді і додаемо на кожному кроці циклу обьект з масиву
 //5 створити в результуючий даті поля entities: і  byId:
 
-const normalizeData = array => {
-  const resultObjectData = {};
-  const objData = {};
-  const arrayId = [];
+// const normalizeData = array => {
+//   const resultObjectData = {};
+//   const objData = {};
+//   const arrayId = [];
 
-  array.forEach(element => {
-    // let elemId = element.id;
-    arrayId.push(element.id);
+//   array.forEach(element => {
+//     // let elemId = element.id;
+//     arrayId.push(element.id);
 
-    console.log(arrayId);
+//     console.log(arrayId);
 
-    objData[element.id] = element;
-  });
+//     objData[element.id] = element;
+//   });
 
-  resaltObjectData['entities'] = objData;
-  resaltObjectData.byId = arrayId;
+//   resaltObjectData['entities'] = objData;
+//   resaltObjectData.byId = arrayId;
 
-  return resultObjectData;
-};
+//   return resultObjectData;
+// };
 
-console.log(normalizeData(inputData));
+// console.log(normalizeData(inputData));
+
+//
+// !New task
+
+// порахуйте всі позитивні або негативні значення і повертає обʼєкт даних
+// 1. Створити функцію яка приймає масив, результатом є об'єкт позитивних та негативних значень
+// 2. Використовуючи reduce на кожному кроці циклу ми перевіряємо позитивне чи негативне значення
+
+
+// const nums = [-90, 45, 0, 16, -18, 24, -67, 43]
+
+// const totalPosAndNegSum = function (arr) {
+//   const resultObj = arr.reduce((arrAcc, element) => {
+//     if (element > 0) {
+//       return {
+//         // ...arrAcc,
+//         pos: arrAcc.pos + element,
+//         neg: arrAcc.neg
+//       };
+//     } else {
+//       return { ...arrAcc, neg: arrAcc.neg + element };
+//   }
+//   }, { pos: 0, neg: 0 })
+//   return resultObj;
+// }
+// console.log(totalPosAndNegSum(nums));
+
+// ! Приклад з kahoot
+
+const numbers = [5, 10, 25, 15];
+const arrayNumbers = numbers.reduce((acc, number) => { console.log('acc', acc); console.log('number', number); });
+console.log(arrayNumbers);
