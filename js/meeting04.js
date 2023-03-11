f6477039c7902de62c5f4918634188113a3fdce3
PolinaTsugankova <pp6056512@gmail.com>
2023-03-11 13:00:34 +0200

    task2

--- js/meeting04.js
+++ js/meeting04.js
@@ -10,31 +10,79 @@
 
 // *? d. клас очікує 1 параметр - об'єкт налаштувань з однойменними властивостями
 
-
-
 // *? Додай метод getInfo(), який повертає рядок:
 
 // *? `Користувачеві ${ім'я} ${вік} років і в нього ${кількістьПублікацій} публікацій.`
 
-
 function User(userObject) {
-    const {userName, age, numbersOfPost } = userObject;
-    this.userName = userName;
-    this.age = age;
-    this.numbersOfPost = numbersOfPost;
+  const { userName, age, numbersOfPost } = userObject;
+  this.userName = userName;
+  this.age = age;
+  this.numbersOfPost = numbersOfPost;
 
-    this.getInfo = function () {
-        console.log(`Користувачеві ${this.userName} ${this.age} років і в нього ${this.numbersOfPost} публікацій.`);
-    }
+  this.getInfo = function () {
+    console.log(
+      `Користувачеві ${this.userName} ${this.age} років і в нього ${this.numbersOfPost} публікацій.`
+    );
+  };
 }
 
+const mango = new User({ userName: 'Mango', age: 36, numbersOfPost: 10 });
+const zina = new User({ userName: 'Zina', age: 26, numbersOfPost: 5 });
 
+console.log('🚀 ~ zina:', zina);
 
-const mango = new User({ userName: "Mango", age: 36, numbersOfPost: 10 });
-const zina = new User({ userName: "Zina", age: 26, numbersOfPost: 5 });
+console.log('🚀 ~ newUser:', mango);
 
-console.log("🚀 ~ zina:", zina)
+//? -------------Task 2 -------------
 
+// Створіть клас Адмін із конструктором, імʼя, пароль.
+// Додати гетер та сетер на пароль, та методи шифтування та перевірки
 
-console.log("🚀 ~ newUser:", mango)
+class Admin {
+  #name;
+  #password;
 
+  constructor(name, password) {
+    this.#name = name;
+    this.#password = password;
+    this.reversePassword = this.#password.split('').reverse().join('');
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  //* Розшифровуємо пароль
+  get password() {
+    return this.reversePassword;
+  }
+
+  //* Зашифровуємо пароль
+  set password(newPassword) {
+    this.#password = newPassword.split('').reverse().join('');
+  }
+
+  checkPassword(newPassword) {
+    if (this.reversePassword === newPassword) {
+      console.log('Check is valid');
+    } else {
+      console.log('You are a stranger');
+    }
+  }
+
+  changePassword(oldPassword, newPassword) {
+    if (this.reversePassword === oldPassword) {
+      this.#password = newPassword.split('').reverse().join('');
+    } else {
+      console.log('Your old password is wrong!');
+    }
+  }
+}
+
+const mango = new Admin('Mango', 'sdvsjdnvskld');
+console.log(mango);
+console.log(mango.name);
+
+console.log(mango.password);
+// console.log(mango.password('Poly'));
